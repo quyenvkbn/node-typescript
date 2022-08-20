216ac99d9f680f189d7fcf29ea0e555c162bb13f
quyenvkbn <12quyen12@gmail.com>
2022-08-21 00:08:02 +0700

    first code

---
+++ .build/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// import db from '#src/models/index.js';
+const demo_1 = __importDefault(require("src/models/demo"));
+console.log(demo_1.default);
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
---
+++ src/config/database.ts
@@ -0,0 +1,24 @@
+const config: any = {
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "node_js",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "root",
+    "database": "node_js",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "root",
+    "database": "node_js",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+export default config
